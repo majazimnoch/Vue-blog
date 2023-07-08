a89df24f84b1759318eb9041165c2480468d6550
majazimnoch <majamzimnoch@gmail.com>
2023-07-08 11:19:44 +0200

    test
    test1

---
+++ app.js
@@ -0,0 +1,13 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+      username: 'Codegirl',
+      newTweet: '',
+      tweets: [
+        'I graduated from Web Development Bootcamp! Huray! I can React, JavaScript, Redux, Html and CSS', 
+        'I made my code portfolio and updated all readmes on Github.', 
+        'Today I start learning Vue. I got this.'
+      ],
+      bio: 'Excited front-end developer.'
+    }
+  });
