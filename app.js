041eb02a525862cc9c8a0195c3b44d48fad2899a
majazimnoch <majamzimnoch@gmail.com>
2023-07-10 20:16:58 +0200

    styling
    changed colour palette. changed theme of the site.

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const app = new Vue({
     el: '#app',
     data: {
-      username: 'Codegirl',
+      username: 'Maja',
       newTweet: '',
       tweets: [
-        'I graduated from Web Development Bootcamp! Huray! I can React, JavaScript, Redux, Html and CSS', 
-        'I made my code portfolio and updated all readmes on Github.', 
-        'Today I start learning Vue. I got this.'
+        "I graduated from Web Development Bootcamp! Huray! Now I can take some days off and relax.",
+        "On my first week of summer I went to my family's summer house nearby a lake. All I was thinking about was this new framework, Vue.",
+        "So I was taking my computer to the beach and I was coding. I made my first blog page using Vue! I got this."
       ],
-      bio: 'Excited front-end developer.'
+      bio: 'Excited front-end developer, currently enjoying her summer break.'
     }
   });
