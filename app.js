4d721f87469ffa0497458bd3552398c9318362f9
majazimnoch <majamzimnoch@gmail.com>
2023-07-11 09:39:10 +0200

    styling
    changed theme, photos, colours

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = new Vue({
       newTweet: '',
       tweets: [
         "I graduated from Web Development Bootcamp! Huray! Now I can take some days off and relax.",
-        "On my first week of summer I went to my family's summer house nearby a lake. All I was thinking about was this new framework, Vue.",
+        "On my first week of summer I went to my family's summer house nearby sea. All I was thinking about was this new framework, Vue.",
         "So I was taking my computer to the beach and I was coding. I made my first blog page using Vue! I got this."
       ],
       bio: 'Excited front-end developer, currently enjoying her summer break.'
